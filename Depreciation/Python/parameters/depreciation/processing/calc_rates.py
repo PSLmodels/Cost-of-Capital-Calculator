31bec66c8659b7099d345d70f5df08f6a081c703
Benjamin Gardner <bfgard@gmail.com>
2016-04-29 10:55:14 -0600

    Updating main scripts

--- Depreciation/Python/parameters/depreciation/processing/calc_rates.py
+++ Depreciation/Python/parameters/depreciation/processing/calc_rates.py
@@ -1,7 +1,7 @@
 """
 Calculate Depreciation Rates (calc_rates):
 -------------------------------------------------------------------------------
-Last updated: 5/26/2015.
+Last updated: 4/29/2016.
 
 This module provides functions for calculating economic and tax depreciation
 rates. All of the data is processed into NAICS trees by other modules, and
@@ -25,8 +25,6 @@ import constants as cst
 _ECON_DEPR_IN_PATH = os.path.join(_DEPR_DIR, "Economic Depreciation Rates.csv")
 _TAX_DEPR_IN_PATH = os.path.join(_DEPR_DIR, "BEA_IRS_Crosswalk.csv")
 
-
-
 def calc_depr_rates(asset_tree, inv_tree, land_tree):
     # Opening file containing depreciation rates by asset type:
     depr_econ = pd.read_csv(_ECON_DEPR_IN_PATH)
@@ -38,18 +36,11 @@ def calc_depr_rates(asset_tree, inv_tree, land_tree):
     #Initialize tree for depreciation rates:
     depr_tree = naics.generate_tree()
     depr_tree.append_all(df_nm="Economic", df_cols=types)
+    #Makes a list of all the assets
+    asset_list = asset_tree.enum_inds[0].data.dfs['Corp'].columns
+    asset_list = asset_list.values.tolist()
     #Runs three times, once for all the assets, once for the corporate assets, and once for non-corporate assets
-    for i in types:
-        #Makes a list of all the assets
-        asset_list = asset_tree.enum_inds[0].data.dfs[i].columns
-        asset_list = asset_list.values.tolist()
-        '''
-        match = np.array([-1] * len(asset_list))
-        for j in xrange(0, asset_tree.enum_inds[0].data.dfs[i].shape[1]):
-            for k in xrange(0, len(econ_assets)):
-                if str(asset_list[j]).strip() == str(econ_assets[k]).strip():
-                    match[j] = k
-        '''
+    for i in types:        
         #Iterates over every industry in the tree       
         for j in xrange(0, len(depr_tree.enum_inds)):
             asset_depreciation = 0
@@ -60,19 +51,71 @@ def calc_depr_rates(asset_tree, inv_tree, land_tree):
             depr_df = depr_tree.enum_inds[j].data.dfs['Economic']
             #Iterates over each asset in the industry
             for k in xrange(0, len(asset_list)):
-                '''
-               if(match[k] == -1):
-                    print k
-                    continue   
-                '''       
                 #calculates the sum of all the depreciation in the industry, multiplying the amount of each asset by its corresponding depreciation rate
                 asset_depreciation += (asset_df.iloc[0,k] * econ_rates[k])
-            '''
-            if(sum(asset_df.iloc[0,:]) != 0):
-                depr_df[i][0] = total_depreciation/sum(asset_df.iloc[0,:])
+            #calculates the total capital stock in the industry
+            tot_assets = sum(asset_tree.enum_inds[j].data.dfs[i].iloc[0,:])
+            tot_inv = inv_tree.enum_inds[j].data.dfs["Inventories"][i][0]
+            tot_land = land_tree.enum_inds[j].data.dfs["Land"][i][0]
+            total_capital_stock = tot_assets + tot_inv + tot_land
+            #
+            #cur_df = depr_tree.enum_inds[j].data.dfs["Economic"]
+            if(total_capital_stock != 0):
+                #calculates the weighted average depreciation rate for assets only (can be changed to include inventories and land)
+                depr_df[i][0] = asset_depreciation / tot_assets
             else:
                 depr_df[i][0] = 0
-            '''
+     
+    return depr_tree
+
+def calc_tax_depr_rates(asset_tree, inv_tree, land_tree):
+    #
+    tax_data = pd.read_csv(_TAX_DEPR_IN_PATH).fillna(0)
+    tax_assets = tax_data["Asset Type"]
+    # Real Interest Rate:
+    r = .05  # NEEDS TO BE AUTOMATED!!!!
+    #
+    tax_mthds = {"GDS 200%": 2.0, "GDS 150%": 1.5, "GDS SL": 1.0, "ADS SL": 1.0}
+    tax_cols = {'Asset','Tax_Depreciation_Rate'}
+    tax_rates = pd.DataFrame(np.zeros((len(tax_assets),len(tax_cols))), columns = tax_cols)
+    tax_rates['Asset'] = tax_assets
+
+    # Compute the tax depreciation rates:
+    for i in xrange(0, len(tax_assets)):
+        tax_method = tax_data['Method'][i]
+        tax_system = tax_data['System'][i]
+        tax_life = tax_data[tax_system][i]
+        tax_b = tax_mthds[tax_method]
+        tax_beta = tax_b / tax_life
+        if(tax_method == 'GDS 200%' or tax_method == 'GDS 150%'):
+            tax_star = tax_life * (1 - (1/tax_b))
+            tax_z = (((tax_beta/(tax_beta+r))* (1-np.exp(-1*(tax_beta+r)*tax_star))) 
+                      + ((np.exp(-1*tax_beta*tax_star)/((tax_life-tax_star)*r))* (np.exp(-1*r*tax_star)-np.exp(-1*r*tax_life))))
+            tax_rates.iloc[i,0] = r/((1/tax_z)-1)
+        else:
+            tax_rates.iloc[i,0] = tax_beta
+    #
+    types = ["All", "Corp", "Non-Corp"]
+    # Initialize tax depreciation rates tree:
+    depr_tree = naics.generate_tree()
+    depr_tree.append_all(df_nm="Tax", df_cols=types)
+    #
+    asset_list = asset_tree.enum_inds[0].data.dfs['Corp'].columns
+    asset_list = asset_list.values.tolist() 
+    for i in types:        
+        #Iterates over every industry in the tree       
+        for j in xrange(0, len(depr_tree.enum_inds)):
+            asset_depreciation = 0
+            total_depreciation = 0
+            #grabs the assets for the industry
+            asset_df = asset_tree.enum_inds[j].data.dfs[i]
+            #grabs the depreciation value in the tree so that it can be set on line 86
+            depr_df = depr_tree.enum_inds[j].data.dfs['Tax']
+            #Iterates over each asset in the industry
+            for k in xrange(0, len(asset_list)):   
+                #calculates the sum of all the depreciation in the industry, multiplying the amount of each asset by its corresponding depreciation rate
+                asset_depreciation += (asset_df.iloc[0,k] * tax_rates['Tax_Depreciation_Rate'][k])
+            
             #calculates the total capital stock in the industry
             tot_assets = sum(asset_tree.enum_inds[j].data.dfs[i].iloc[0,:])
             tot_inv = inv_tree.enum_inds[j].data.dfs["Inventories"][i][0]
@@ -86,100 +129,5 @@ def calc_depr_rates(asset_tree, inv_tree, land_tree):
                 depr_df[i][0] = asset_depreciation / tot_assets
             else:
                 depr_df[i][0] = 0
-            
-            
-        
-       #Inventories and land have an approximately zero depreciation rate:
-        '''
-        for j in xrange(0, len(depr_tree.enum_inds)):
-            tot_assets = sum(asset_tree.enum_inds[j].data.dfs["All"].iloc[0,:])
-            tot_inv = inv_tree.enum_inds[j].data.dfs["Inventories"]["All"][0]
-            tot_land = land_tree.enum_inds[j].data.dfs["Land"]["All"][0]
-            if(tot_assets+tot_inv+tot_land == 0):
-                continue
-            ratio = tot_assets / (tot_assets + tot_inv + tot_land)
-            #
-            cur_df = depr_tree.enum_inds[j].data.dfs["Economic"]
-            cur_df[i][0] = ratio * cur_df[i][0]
-        '''
-
-    return depr_tree
-
-
-'''
-
-'''
-def calc_tax_depr_rates(asset_tree, inv_tree, land_tree):
-    #
-    tax_data = pd.read_csv(_TAX_DEPR_IN_PATH).fillna(0)
-    tax_assets = tax_data["Asset Type"]
-    # Real Interest Rate:
-    r = .05  # NEEDS TO BE AUTOMATED!!!!
-    #
-    tax_gds_mthds = {"GDS 200%": 2.0, "GDS 150%": 1.5, "GDS SL": 1.0}
-    tax_ads_mthds = {"ADS SL": 1.0}
-    tax_cols = tax_gds_mthds.keys() + tax_ads_mthds.keys()
-    tax_systems = {"GDS": tax_gds_mthds, "ADS": tax_ads_mthds}
-    tax_rates = pd.DataFrame(np.zeros((len(tax_assets),len(tax_cols))), 
-                             columns = tax_cols)
-    tax_rates["Asset"] = tax_assets
-    # Compute the tax rates:
-    for i in tax_systems:
-        tax_yrs = tax_data[i]
-        for j in tax_systems[i]:
-            tax_b = tax_systems[i][j]
-            tax_beta = tax_b/tax_yrs
-            tax_star = tax_yrs * (1 - (1/tax_b))
-            tax_z = (((tax_beta/(tax_beta+r))*
-                      (1-np.exp(-1*(tax_beta+r)*tax_star))) 
-                      + ((np.exp(-1*tax_beta*tax_star)/
-                      ((tax_yrs-tax_star)*r))*
-                      (np.exp(-1*r*tax_star)-np.exp(-1*r*tax_yrs))))
-            tax_rates[j] = r/((1/tax_z)-1)
-    tax_rates = tax_rates.fillna(0)
-    #
-    types = ["All", "Corp", "Non-Corp"]
-    # Initialize tax depreciation rates tree:
-    depr_tree = naics.generate_tree()
-    for j in tax_systems:
-        for k in tax_systems[j]:
-            depr_tree.append_all(df_nm=k, df_cols=types)
-    depr_tree.append_all(df_nm="Recommended", df_cols=types)
-    # 
-    for i in types:
-        asset_list = asset_tree.enum_inds[0].data.dfs[i].columns
-        asset_list = asset_list.values.tolist()
-        match = np.array([-1] * len(asset_list))
-        for j in xrange(0, asset_tree.enum_inds[0].data.dfs[i].shape[1]):
-            for k in xrange(0, len(tax_assets)):
-                if str(asset_list[j]).strip() == str(tax_assets[k]).strip():
-                    match[j] = k
-        for j in xrange(0, len(depr_tree.enum_inds)):
-            cur_ind = depr_tree.enum_inds[j]
-            asset_df = asset_tree.enum_inds[j].data.dfs[i]
-            #
-            tot_assets = sum(asset_tree.enum_inds[j].data.dfs[i].iloc[0,:])
-            tot_inv = inv_tree.enum_inds[j].data.dfs["Inventories"][i][0]
-            tot_land = land_tree.enum_inds[j].data.dfs["Land"][i][0]
-            if(tot_assets+tot_inv+tot_land == 0):
-                continue
-            ratio = tot_assets / (tot_assets + tot_inv + tot_land)
-            #
-            for k in tax_cols:
-                cur_tax = cur_ind.data.dfs[k][i]
-                cur_sum = 0.0
-                for l in xrange(0, len(asset_list)):
-                    if(match[l] == -1):
-                        continue
-                    cur_sum += (asset_df.iloc[0,l] * tax_rates[k][match[l]])
-                cur_tax[0] = ratio * (cur_sum/sum(asset_df.iloc[0,:]))
-            #
-            cur_tax = cur_ind.data.dfs["Recommended"][i]
-            cur_sum = 0
-            for l in xrange(0, len(asset_list)):
-                if(match[l] == -1):
-                    continue
-                cur_rate = tax_rates[tax_data["Method"][match[l]]][match[l]]
-                cur_sum += asset_df.iloc[0,l] * cur_rate
-            cur_tax[0] = ratio * (cur_sum/sum(asset_df.iloc[0,:]))
+                    
     return depr_tree
