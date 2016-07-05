7563eaefbc24b598f59472be2af3c3ce3ce15bd5
Benjamin Gardner <bfgard@gmail.com>
2016-07-05 16:42:55 -0400

    addition of industry cost of capital calc

--- Python/btax/cost_of_capital.py
+++ Python/btax/cost_of_capital.py
@@ -21,28 +21,71 @@ def asset_cost_of_capital(fixed_assets):
 	econ_depr = econ_depr.drop('Code',1)
 	tax_depr_allow = pd.read_csv(_TAX_DEPR_FILE)
 	depr_rates = np.array(econ_depr.merge(tax_depr_allow))
+	# grabs the constant values from the parameters dictionary
 	params = param.get_params()
 	inflation_rate = params['Inflation rate']
 	corp_tax = params['Corporate tax rate']
 	non_corp_tax = params['Non corporate tax rate']
 	discount_rate = params['Discount rate']
-	types = ['Corp', 'Non_corp']
+	types = ['corp', 'non_corp']
 
+	# calculates the cost of capital by asset type 
 	cost_of_capital = pd.DataFrame(index=np.arange(0,len(depr_rates)), columns=types)
 	metr = pd.DataFrame(index=np.arange(0,len(depr_rates)), columns=types)
 	for j in types:
-		if(j == 'Corp'):
+		# chooses the corporate or non-corporate tax for the statutory tax rate
+		if(j == 'corp'):
 			tax_rate = corp_tax
 		else:
 			tax_rate = non_corp_tax
 		for i in xrange(0, len(cost_of_capital)):
+			# calculates the cost of capital using input parameters and depreciation rates
 			cost_of_capital[j][i] = ((discount_rate - inflation_rate) + depr_rates[i][1]) * (1 - tax_rate * depr_rates[i][2]) / (1 - tax_rate) - depr_rates[i][1]
+			# also calculates the marginal effective tax rate by asset and entity type
 			metr[j][i] = (cost_of_capital[j][i] - discount_rate + inflation_rate) / cost_of_capital[j][i]
-	import ipdb
-	ipdb.set_trace()	
-	naics_codes = pd.read_csv(_NAICS_PATH)['2007 NAICS Codes'].tolist()[1:]
+
 	keys = fixed_assets.keys()
-	return cost_of_capital
+
+	# aggregates the sub industries up to the 2 digit NAICS code level
+	agg_fa = {}
+	for i in types:
+		for key in keys:
+			if(agg_fa.has_key(key[:2]) and agg_fa[key[:2]].has_key(i)):
+				agg_fa[key[:2]][i] += fixed_assets[key][i]
+			elif(agg_fa.has_key(key[:2]) and not agg_fa[key[:2]].has_key(i)):
+				agg_fa[key[:2]].update({i: fixed_assets[key][i]})
+			else:
+				agg_fa[key[:2]] = {i: fixed_assets[key][i]}
+
+	# handles the exceptions where an industry code covers multiple sub industries, summing them together
+	exceptions = {'31-33': ['31','32','33'], '44-45': ['44'], '48-49': ['48', '49']}			
+	for exc, children in exceptions.iteritems():
+		for child_ind in children:
+			for i in types:
+				if(agg_fa.has_key(exc)):
+					if(agg_fa[exc].has_key(i)):
+						agg_fa[exc][i] += agg_fa[child_ind][i]
+					else:
+						agg_fa[exc].update({i: agg_fa[child_ind][i]})
+				else:
+					agg_fa[exc] = {i: agg_fa[child_ind][i]}
+			del agg_fa[child_ind]
+
+	cc_agg = pd.DataFrame(index=np.arange(0, len(agg_fa)), columns=['NAICS', 'corp', 'non_corp'])
+	asst_cc = {}
+	# calculates the cost of capital by industry and entity type
+	for i in types:
+		asst_cc[i] = np.array(cost_of_capital[i])
+	l = 0
+	for code, dictionary in agg_fa.iteritems():
+		cc_agg['NAICS'][l] = code
+		for i in types:
+			cc_agg[i][l] = sum(dictionary[i] * asst_cc[i]) / sum(dictionary[i])
+		l += 1
+
+	cc_agg = cc_agg.sort('NAICS')
+	cc_agg.fillna(0)
+	return cc_agg
 
 
 def calc_cost_of_capital(depr_params, discount_rates):
