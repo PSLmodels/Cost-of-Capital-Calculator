55342d6aedea8b83794c97d7540b03f840784f84
Jason DeBacker <jason.debacker@A02655JDEBACKER.local>
2016-09-15 09:34:32 -0400

    run reform that is bonus phaseout

--- btax/btax_reform_run.py
+++ btax/btax_reform_run.py
@@ -3,11 +3,22 @@ Runs a paricular reform against the baseline policy:
 -------------------------------------------------------------------------------
 """
 # Import packages
-from btax.run_btax import run_btax
+from btax.run_btax import run_btax, run_btax_with_baseline_delta, run_btax_to_json_tables
 
 test_run = False # flag for test run (for Travis CI)
 start_year = 2016
 iit_reform = {}
 
 # run btax
-run_btax(test_run,False,start_year,iit_reform,btax_other_corpeq=0.5)
+# #2018 law
+# run_btax_with_baseline_delta(test_run,False,start_year,iit_reform,btax_depr_3yr_exp=40.,
+#          btax_depr_5yr_exp=40.,btax_depr_7yr_exp=40.,btax_depr_10yr_exp=40.,
+#          btax_depr_15yr_exp=40.,btax_depr_20yr_exp=40.)
+#2019 law
+run_btax_to_json_tables(test_run,False,start_year,iit_reform,btax_depr_3yr_exp=30.,
+         btax_depr_5yr_exp=30.,btax_depr_7yr_exp=30.,btax_depr_10yr_exp=30.,
+         btax_depr_15yr_exp=30.,btax_depr_20yr_exp=30.)
+# #2020 law
+# run_btax_with_baseline_delta(test_run,False,start_year,iit_reform,btax_depr_3yr_exp=0.,
+#          btax_depr_5yr_exp=0.,btax_depr_7yr_exp=0.,btax_depr_10yr_exp=0.,
+#          btax_depr_15yr_exp=0.,btax_depr_20yr_exp=0.)
