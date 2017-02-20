773e43b58fd7d50650bca8133f740d1b3022c910
Jason DeBacker <jason.debacker@A02655JDEBACKER.local>
2017-02-20 15:54:06 -0500

    update test for new file name

--- btax/tests/test_params_have_effect.py
+++ btax/tests/test_params_have_effect.py
@@ -4,7 +4,7 @@ import re
 import pytest
 
 from btax.parameters import DEFAULTS, get_params, translate_param_names
-from btax.run_btax import run_btax_to_json_tables
+from btax.execute import runner
 import btax.front_end_util as front_end
 
 front_end.DO_ASSERTIONS = True # Override env var
@@ -14,7 +14,7 @@ def tst_once(fast_or_slow, **user_params):
     if fast_or_slow == 'slow':
         # actually run the model
         # and look at the "changed" tables
-        tables = run_btax_to_json_tables(**user_params)
+        tables = runner(**user_params)
         assert isinstance(tables, dict)
         for k, v in tables.items():
             if k in ('row_grouping', 'result_years',):
