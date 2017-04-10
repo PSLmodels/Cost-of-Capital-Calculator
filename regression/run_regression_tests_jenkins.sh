679b2a82cf21e6971797eeed4187bca3dacbbcc7
Peter Steinberg <psteinberg@continuum.io>
2017-04-10 16:54:30 -0700

    regression test python version fix

--- regression/run_regression_tests_jenkins.sh
+++ regression/run_regression_tests_jenkins.sh
@@ -1,3 +1,4 @@
 export MAKE_MINICONDA=1
 export REGRESSION_DIR=`pwd`
+export TRAVIS_PYTHON_VERSION=2.7
 . .run_regression_tests.sh
--- regression/run_regression_tests_local.sh
+++ regression/run_regression_tests_local.sh
@@ -1,3 +1,6 @@
 export MAKE_MINICONDA=
 export REGRESSION_DIR=`pwd`
+if [ "$TRAVIS_PYTHON_VERSION" = "" ];then
+    export TRAVIS_PYTHON_VERSION=2.7
+fi
 . .run_regression_tests.sh
