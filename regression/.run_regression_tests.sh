89a0966161aa21823474b0202d5f548114dd3532
Peter Steinberg <psteinberg@continuum.io>
2017-05-08 11:56:05 -0700

    more diagnostics - add semicolons to bash

--- regression/.run_regression_tests.sh
+++ regression/.run_regression_tests.sh
@@ -19,17 +19,18 @@ for BTAX_VERSION in "${@}";do
     if [ "$BTAX_REFORMS_FILE" = "" ];then
         export BTAX_REFORMS_FILE="reforms_04_03_2017.json";
     fi
-    rm -f asset_data.pkl
-    export dt_str=$(echo $(date) | tr ":" "_" | tr " " "_")
-    export output_file="regression_test_out_${dt_str}"
+    rm -f asset_data.pkl;
+    export dt_str=$(echo $(date) | tr ":" "_" | tr " " "_");
+    export output_file="regression_test_out_${dt_str}";
     # Echo the output and write it to file
-    conda clean -pt
-    echo Run regression tests on $(date) in PWD:
-    echo `pwd`
-    echo With files: `ls -l`
-    echo Conda "env" list: `conda env list`
+    conda clean -pt;
+    echo Run regression tests on $(date) in PWD:;
+    echo `pwd`;
+    echo With files: `ls -l`;
+    echo Conda "env" list: `conda env list`;
     echo Conda list: `conda list`;
-    cd $REGRESSION_DIR && python run_regression_tests.py $BTAX_REFORMS_FILE | tee $output_file
+    echo git branch: `git branch`;
+    cd $REGRESSION_DIR && python run_regression_tests.py $BTAX_REFORMS_FILE | tee $output_file;
 done
 
 
