2bc226dbf1c1f539eb787e0bf4d867bd418ce672
jdebacker <jason.debacker@gmail.com>
2018-01-30 11:16:03 -0500

    test npv function

--- btax/tests/test_calc_z.py
+++ btax/tests/test_calc_z.py
@@ -16,8 +16,45 @@ def test_calc_tax_depr_rates():
 
 
 def test_npv_tax_deprec():
+    df = pd.DataFrame({'Asset': ['Land', 'Inventories', 'IP', 'Structures',
+                                 'Equipment'],
+                       'Amount': [1000, 500, 100, 200, 200],
+                       'bonus': [0.0, 0.0, 0.4, 1.0, 0.9],
+                       'delta': [0.01, 0.1, 0.1, 0.02, 0.1],
+                       'ADS Life': [40, 1, 10, 20, 8],
+                       'GDS Life': [40, 3, 10, 20, 8],
+                       'Method': ['DB 200%', 'DB 150%', 'SL',
+                                  'Expensing', 'Economic']})
+    r = np.array([[0.05, 0.06], [0.04, .03], [0.11, 0.12]])
+    pi = 0.03
+    tax_methods = {'DB 200%': 2.0, 'DB 150%': 1.5, 'SL': 1.0,
+                   'Economic': 1.0, 'Expensing': 1.0}
+    financing_list = ['', '_d', '_e']
+    entity_list = ['_c', '_nc']
+    test_exp_df = calc_z.npv_tax_deprec(df, r, pi, tax_methods, financing_list, entity_list)
 
-    assert
+    results_df = pd.DataFrame({'Asset': ['Land', 'Inventories', 'IP',
+                                         'Equipment', 'Structures'],
+                       'Amount': [1000, 500, 100, 200, 200],
+                       'bonus': [0.0, 0.0, 0.4, 0.9, 1.0],
+                       'delta': [0.01, 0.1, 0.1, 0.1, 0.02],
+                       'ADS Life': [40, 1, 10, 8, 20],
+                       'GDS Life': [40, 3, 10, 8, 20],
+                       'Method': ['DB 200%', 'DB 150%', 'SL',
+                                  'Economic', 'Expensing'],
+                       'b': [2.0, 1.5, 1.0, 1.0, 1.0],
+                       'z_c': [0.517881, 0.933631, 0.872163, 0.983333, 1.0],
+                       'z_c_d': [0.577504, 0.946392, 0.894520, 0.990909, 1.0],
+                       'z_c_e': [0.316237, 0.862037, 0.763888, 0.955556, 1.0],
+                       'z_nc': [0.468705, 0.921116, 0.851188, 0.976923, 1.0],
+                       'z_nc_d': [0.650637, 0.959403, 0.918364, 1.0, 1.0],
+                       'z_nc_e': [0.296946, 0.850885, 0.749403, 0.952632, 1.0]})
+    results_df = results_df[['ADS Life', 'Amount', 'Asset', 'GDS Life',
+                             'Method', 'bonus', 'delta', 'b', 'z_c',
+                             'z_nc', 'z_c_d', 'z_nc_d', 'z_c_e',
+                             'z_nc_e']]
+
+    assert_frame_equal(test_exp_df, results_df, check_dtype=False)
 
 
 def test_dbsl():
@@ -57,7 +94,7 @@ def test_sl():
     df = pd.DataFrame({'Asset': ['Land', 'Inventories', 'IP', 'Structures',
                                  'Equipment'],
                        'Amount': [1000, 500, 100, 200, 200],
-                       'bonus': [0.0, 0.0, 0.4, 1.0, 0.9],
+                       'bonus': [0.0, 0.0, 0.4, 1.0, 1.2],
                        'ADS Life': [40, 1, 10, 20, 8]})
     r = np.array([[0.05, 0.06], [0.04, .03], [0.11, 0.12]])
     financing_list = ['', '_d', '_e']
