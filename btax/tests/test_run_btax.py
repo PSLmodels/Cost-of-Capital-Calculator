bb9cf7c62cfa2456f26419bee4be491cd79cf63b
jdebacker <jason.debacker@gmail.com>
2018-02-18 16:40:02 -0500

    use current dir

--- btax/tests/test_run_btax.py
+++ btax/tests/test_run_btax.py
@@ -1,18 +1,23 @@
 import pytest
+import os
 import pickle
 import pandas as pd
 from pandas.testing import assert_frame_equal
 from btax import run_btax
+os.path.abspath(os.path.dirname(__file__))
 
+CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 
 # Load input values
-input_tuple = pickle.load(open('./tests/run_btax_baseline_inputs.pkl', 'rb'),
-                          encoding='Latin')
+input_tuple = pickle.load(open(
+    os.path.join(CUR_PATH, './tests/run_btax_baseline_inputs.pkl'),
+    'rb'), encoding='Latin')
 test_run, baseline, start_year, iit_reform, user_params = input_tuple
 # Load pickle with results to check against
 (result_by_asset, result_by_industry) =\
-    pickle.load(open('./tests/run_btax_baseline_outputs.pkl', 'rb'),
-                encoding='Latin')
+    pickle.load(open(
+        os.path.join(CUR_PATH, './tests/run_btax_baseline_outputs.pkl'),
+        'rb'), encoding='Latin')
 # Run B-Tax with these inputs
 test_by_asset, test_by_industry = run_btax.run_btax(
     test_run, baseline, start_year, iit_reform, **user_params)
