4eb30fe416821cc3bad9fa0212bad5204494f2da
jdebacker <jason.debacker@gmail.com>
2018-11-14 16:23:25 -0500

    update setup and init scripts

--- btax/__init__.py
+++ btax/__init__.py
@@ -1,6 +1,6 @@
+"""
+Specify what is available to import from the btax package.
+"""
 from btax.parameters import DEFAULTS, DEFAULT_ASSET_COLS, DEFAULT_INDUSTRY_COLS
-from btax import _version
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+__version__ = '0.0.0'
