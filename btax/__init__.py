5f5c4253edd42d2fa2f442cc8c42d0ee56ec4d58
jdebacker <jason.debacker@gmail.com>
2019-02-19 22:02:06 -0500

    change available packages when btax imported

--- btax/__init__.py
+++ btax/__init__.py
@@ -1,6 +1,8 @@
 """
 Specify what is available to import from the btax package.
 """
-from btax.parameters import DEFAULTS, DEFAULT_ASSET_COLS, DEFAULT_INDUSTRY_COLS
+from btax.parameters import *
+from btax.parametersbase import *
+from btax.records import *
 
 __version__ = '0.0.0'
