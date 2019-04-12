333286f6bde7a9eebb5d680829ccbb91df621267
jdebacker <jason.debacker@gmail.com>
2019-04-11 22:05:28 -0400

    update run example to use new API

--- run_examples/run_ccc_example.py
+++ run_examples/run_ccc_example.py
@@ -3,34 +3,54 @@ Runs Cost-of-Capital-Calculator with TCJA as baseline and 2017 law as
 the reform.
 ------------------------------------------------------------------------
 """
-# Import packages
-from ccc.run_ccc import run_ccc_with_baseline_delta
-from taxcalc import *
+# Import packages and classes
+import ccc
+import taxcalc
+from ccc.data import Assets
 from ccc.parameters import Specifications
+from ccc.calculator import Calculator
 
-test_run = False  # flag for test run (for Travis CI)
-start_year = 2018
-
+# Read in a reform to compare against
 # Note that TCJA is current law baseline in TC 0.16+
 # Thus to compare TCJA to 2017 law, we'll use 2017 law as the reform
 reform_url = ('https://raw.githubusercontent.com/'
               'PSLmodels/Tax-Calculator/master/taxcalc/'
               'reforms/2017_law.json')
-ref = Calculator.read_json_param_objects(reform_url, None)
+ref = taxcalc.Calculator.read_json_param_objects(reform_url, None)
 iit_reform = ref['policy']
 
-# Initialize Cost-of-Capital-Calculator parameters
-baseline_parameters = Specifications(year=2018, call_tc=True,
-                                     iit_reform={})
+# Initialize Asset and Calculator Objects
+assets = Assets()
+# Baseline
+baseline_parameters = Specifications(year=2018, call_tc=True, iit_reform={})
+calc1 = Calculator(baseline_parameters, assets)
+# Reform
 reform_parameters = Specifications(year=2018, call_tc=True,
                                    iit_reform=iit_reform)
+business_tax_adjustments = {
+    'CIT_rate': 0.35, 'BonusDeprec_3yr': 0.50, 'BonusDeprec_5yr': 0.50,
+    'BonusDeprec_7yr': 0.50, 'BonusDeprec_10yr': 0.50,
+    'BonusDeprec_15yr': 0.50, 'BonusDeprec_20yr': 0.50}
+reform_parameters.update_specifications(business_tax_adjustments)
+calc2 = Calculator(reform_parameters, assets)
 
-reform_params = {'CIT_rate': 0.35, 'BonusDeprec_3yr': 0.50,
-                 'BonusDeprec_5yr': 0.50, 'BonusDeprec_7yr': 0.50,
-                 'BonusDeprec_10yr': 0.50, 'BonusDeprec_15yr': 0.50,
-                 'BonusDeprec_20yr': 0.50}
-reform_parameters.update_specifications(reform_params)
+# Do calculations by asset
+base_assets_df = calc1.calc_by_asset()
+reform_assets_df = calc2.calc_by_asset()
+# Do Calculations by Industry
+base_industry_df = calc1.calc_by_industry()
+reform_industry_df = calc2.calc_by_industry()
 
-# Run Cost-of-Capital-Calculator
-run_ccc_with_baseline_delta(baseline_parameters, reform_parameters,
-                            data='cps')
+# Generate dataframes with differences
+diff_assets_df = ccc.utils.diff_two_tables(
+    reform_assets_df, base_assets_df)
+diff_industry_df = ccc.utils.diff_two_tables(
+    reform_industry_df, base_industry_df)
+
+# Save dataframes to disk as csv files
+base_industry_df.to_csv('baseline_byindustry.csv', encoding='utf-8')
+reform_industry_df.to_csv('reform_byindustry.csv', encoding='utf-8')
+base_assets_df.to_csv('baseline_byasset.csv', encoding='utf-8')
+reform_assets_df.to_csv('reform_byasset.csv', encoding='utf-8')
+diff_industry_df.to_csv('changed_byindustry.csv', encoding='utf-8')
+diff_assets_df.to_csv('changed_byasset.csv', encoding='utf-8')
