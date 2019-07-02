67f7e1b6c37ac1643791e5ae719a2f04c964009a
jdebacker <jason.debacker@gmail.com>
2019-07-02 17:23:23 -0400

    test of str_modified func

--- ccc/tests/test_utils.py
+++ ccc/tests/test_utils.py
@@ -9,5 +9,18 @@ def test_to_str():
     '''
     number = '3'
     test_str = utils.to_str(number)
-    print('test_str = ', test_str)
     assert isinstance(test_str, str)
+
+
+test_data = [(27.5, '27_5'), (30, '30')]
+
+
+@pytest.mark.parametrize('number,expected', test_data,
+                         ids=['Decimal', 'Integer'])
+def test_str_modified(number, expected):
+    '''
+    Test of the str_modified() function
+    '''
+    number = '3'
+    test_str = utils.str_modified(number)
+    assert (number == test_str)
