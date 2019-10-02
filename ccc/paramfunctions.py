67e781b3047b50b3fb9d9f0b5441289db9f58ae2
jdebacker <jason.debacker@gmail.com>
2019-10-01 21:30:45 -0400

    break up into more functions

--- ccc/paramfunctions.py
+++ ccc/paramfunctions.py
@@ -53,6 +53,152 @@ def calc_s_c_d_td(gamma, i, pi, sprime_c_td):
     return s_c_d_td
 
 
+def calc_s__d(s_d_td, alpha_d_ft, alpha_d_td, alpha_d_nt, tau_int,
+              tau_w, i, pi):
+    r'''
+    Compute the after-tax return to debt investments.
+
+    ..math::
+        s_{j,d} = \alpha_{j,d,ft}((1-\tau_{int})i - \pi) + \alpha_{j,d,td}s_{j,d,td} + \alpha_{j,d,nt}(i-\pi) - \tau_{w}
+
+    Args:
+        s_d_td (scalar): after-tax return on debt investments made
+            through tax-deferred acounts
+        alpha_d_ft (scalar): fraction of debt investments held in
+            full-tax accounts
+        alpha_d_td (scalar): fraction of debt investments held in
+            tax-deferred acounts
+        alpha_d_nt (scalar): fraction of debt investments held in
+            tax-free accounts
+        tau_int (scalar): marginal tax rate on interest income
+        tau_w (scalar): marginal tax rate on wealth
+        i (scalar): nominal interest rate
+        pi (scalar): inflation rate
+
+    Returns:
+        s__d (scalar): after-tax return on debt investments
+    '''
+    s__d = (alpha_d_ft * (((1 - tau_int) * i) - pi) + alpha_d_td *
+            s_d_td + alpha_d_nt * (i - pi) - tau_w)
+
+    return s__d
+
+
+def calc_g__g(Y_g, tau_cg, m, E_c, pi):
+    r'''
+    Calculate the real, after-tax annualized return on short or long-
+    term capital gains
+
+    ..math::
+        g_{icg} = \frac{1}{Y_{icg}}\ln\biggl[(1-\tau_{icg})e^{(\pi+mE)Y_{icg}}+\tau_{icg}\biggr] + \pi
+
+    Args:
+        Y_g (scalar): number of years asset held before gains realized
+        tau_cg (scalar): tax rate on capital gains income
+        m (scalar): dividend payout rate
+        E_c (scalar): expected, after-tax return on corporate equity
+        pi (scalar): inflation rate
+
+    Returns:
+        g__g (scalar): real, after-tax annualized return on capital
+            gains
+    '''
+    g__g = (
+        (1 / Y_g) * np.log(((1 - tau_cg) * np.exp((pi + m * E_c) *
+                                                  Y_g)) + tau_cg) - pi)
+
+    return g__g
+
+
+def calc_g(g_scg, g_lcg, omega_scg, omega_lcg, omega_xcg, m, E_c):
+    r'''
+    Calculate the after-tax, annualized, real rate of return on all
+    capital gains
+
+    ..math::
+        g = \omega_{scg}\times g_{scg} + \omega_{lcg}\times g_{lcg} + \omega_{xcg}\times mE
+
+    Args:
+        g_scg (scalar): the real, after-tax annualized return on short-
+            term capital gains
+        g_lcg (scalar): the real, after-tax annualized return on long-
+            term capital gains
+        omega_scg (scalar): the fraction of capital gains that are
+            short-term
+        omega_lcg (scalar): the fraction of capital gains that are
+            long-term
+        m (scalar): dividend payout rate
+        E_c (scalar): expected, after-tax return on corporate equity
+
+    Returns:
+        g (scalar): the after-tax, annualized, real rate of return on
+            all capital gains
+    '''
+    g = omega_scg * g_scg + omega_lcg * g_lcg + omega_xcg * m * E_c
+
+    return g
+
+
+def calc_s_c_e_td(Y_td, tau_td, i, pi, E_c):
+    r'''
+    Calculate the after-tax return on investmentes in corporate equity
+    in tax-deferred accounts.
+
+    ..math::
+        s_{c,e,td} = \frac{1}{Y_{td}}\ln((1-\tau_{td})e^{(\pi+E)Y_{td}}+\tau_{td}) - \pi
+
+    Args:
+        Y_td (scalar): years investments are held in tax-deferred
+            accounts
+        tau_td (scalar): marginal tax rate on investments in
+            tax-deferred accounts
+        i (scalar): nominal interest rate
+        pi (scalar): inflation rate
+        E_c (scalar): expected, after-tax return on corporate equity
+
+    Returns:
+        s_c_e_td (scalar): the after-tax return on investmentes in
+            corporate equity in tax-deferred accounts.
+    '''
+    s_c_e_td = (
+        (1 / Y_td) * np.log(((1 - tau_td) *
+                             np.exp((pi + E_c) * Y_td)) + tau_td) - pi)
+
+    return s_c_e_td
+
+
+def calc_s_c_e(s_c_e_ft, s_c_e_td, alpha_c_e_ft, alpha_c_e_td,
+               alpha_c_e_nt, tau_w, E_c):
+    r'''
+    Calculate the after-tax return on investments in corporate equity
+
+    ..math::
+        s_{c,e} = \alpha_{c,e,ft}\times s_{c,e,ft} + \alpha_{c,e,td}\times s_{c,e,td} + \alpha_{c,e,nt}\times E - \tau_{w}
+
+    Args:
+        s_c_e_ft (scalar): after-tax return on investments in corporate
+            equity in fully-taxable accounts
+        s_c_e_td (scalar): after-tax return on investments in corporate
+            equity in tax-deferred accounts
+        alpha_c_e_ft (scalar): fraction of corporate equity investments
+            made through full-taxable accounts
+        alpha_c_e_td (scalar): fraction of corporate equity investments
+            made through tax-deferred accounts
+        alpha_c_e_nt (scalar): fraction of corporate equity investments
+            made through tax-exempt accounts
+        tau_w (scalar): marginal tax rate on wealth
+        E_c (scalar): expected, after-tax return on corporate equity
+
+    Returns:
+        s_c_e (scalar): the after-tax return on investments in
+            corporate equity
+    '''
+    s_c_e = (alpha_c_e_ft * s_c_e_ft + alpha_c_e_td * s_c_e_td +
+             alpha_c_e_nt * E_c - tau_w)
+
+    return s_c_e
+
+
 def calc_s(p):
     '''
     Compute the after-tax rate of return to savers, s.
