5e21f6d91397ae8214322fd9156148072ae855b5
jdebacker <jason.debacker@gmail.com>
2019-12-20 14:11:56 -0500

    add tests of additional methods

--- ccc/tests/test_calculator.py
+++ ccc/tests/test_calculator.py
@@ -5,93 +5,73 @@ from ccc.data import Assets
 from ccc.calculator import Calculator
 
 
-def test_range_plot():
+def test_calc_other():
     '''
-    Test range_plot method.
-    '''
-    assets = Assets()
-    p = Specification()
-    calc = Calculator(p, assets)
-    p2 = Specification(year=2026)
-    p2.update_specification({'CIT_rate': 0.25})
-    calc2 = Calculator(p2, assets)
-    fig = calc.range_plot(calc2)
-    assert fig
-    fig = calc.range_plot(calc2, output_variable='rho')
-    assert fig
-
-
-def test_grouped_bar():
-    '''
-    Test grouped_bar method.
+    Test calc_other method
     '''
     assets = Assets()
     p = Specification()
     calc = Calculator(p, assets)
-    p2 = Specification(year=2026)
-    p2.update_specification({'CIT_rate': 0.25})
-    calc2 = Calculator(p2, assets)
-    fig = calc.grouped_bar(calc2)
-    assert fig
-    fig = calc.grouped_bar(calc2, output_variable='rho',
-                           group_by_asset=False)
-    assert fig
+    df = calc.calc_by_asset()
+    calc_other_df = calc.calc_other(df)
+    assert ('ucc_mix' in calc_other_df.keys())
+    assert ('metr_mix' in calc_other_df.keys())
+    assert ('mettr_mix' in calc_other_df.keys())
+    assert ('tax_wedge_mix' in calc_other_df.keys())
+    assert ('eatr_mix' in calc_other_df.keys())
 
 
-def test_asset_bubble():
+def test_calc_base():
     '''
-    Test asset bubble plot method.
-    '''
-    assets = Assets()
-    p = Specification()
-    calc = Calculator(p, assets)
-    p2 = Specification(year=2026)
-    p2.update_specification({'CIT_rate': 0.25})
-    calc2 = Calculator(p2, assets)
-    fig = calc.asset_bubble(calc2)
-    assert fig
-    fig = calc.asset_bubble(calc2, output_variable='rho_mix')
-    assert fig
-
-
-def test_bubble_widget():
-    '''
-    Test asset bubble plot method.
+    Test calc_base method
     '''
     assets = Assets()
     p = Specification()
     calc = Calculator(p, assets)
-    p2 = Specification(year=2026)
-    p2.update_specification({'CIT_rate': 0.25})
-    calc2 = Calculator(p2, assets)
-    fig = calc.bubble_widget(calc2)
-    assert fig
+    calc.calc_base()
+    calc_base_df = calc._Calculator__assets.df
+    assert ('z_mix' in calc_base_df.keys())
+    assert ('rho_mix' in calc_base_df.keys())
+
+
+def test_calc_all():
+    '''
+    Test calc_all method
+    '''
+    assets = Assets()
+    p = Specification()
+    calc = Calculator(p, assets)
+    calc.calc_all()
+    calc_all_df = calc._Calculator__assets.df
+    assert ('z_mix' in calc_all_df.keys())
+    assert ('rho_mix' in calc_all_df.keys())
+    assert ('ucc_mix' in calc_all_df.keys())
+    assert ('metr_mix' in calc_all_df.keys())
+    assert ('mettr_mix' in calc_all_df.keys())
+    assert ('tax_wedge_mix' in calc_all_df.keys())
+    assert ('eatr_mix' in calc_all_df.keys())
 
 
 def test_calc_by_asset():
     '''
-    Test calc_by_asset method.
+    Test calc_by_asset method
     '''
-    cyr = 2018
     assets = Assets()
-    p = Specification(year=cyr)
-    calc1 = Calculator(p, assets)
-    assert calc1.current_year == cyr
-    asset_df = calc1.calc_by_asset()
-    assert isinstance(asset_df, pd.DataFrame)
+    p = Specification()
+    calc = Calculator(p, assets)
+    asset_df = calc.calc_by_asset()
+    assert ('major_asset_group' in asset_df.keys())
 
 
 def test_calc_by_industry():
     '''
-    Test calc_by_industry method.
+    Test calc_by_industry method
     '''
-    cyr = 2018
     assets = Assets()
-    p = Specification(year=cyr)
-    calc1 = Calculator(p, assets)
-    assert calc1.current_year == cyr
-    asset_df = calc1.calc_by_industry()
-    assert isinstance(asset_df, pd.DataFrame)
+    p = Specification()
+    calc = Calculator(p, assets)
+    ind_df = calc.calc_by_industry()
+    assert ('major_industry' in ind_df.keys())
 
 
 def test_summary_table():
@@ -153,3 +133,66 @@ def test_industry_summary_table():
     assert calc2.current_year == cyr
     ind_df = calc1.industry_summary_table(calc2)
     assert isinstance(ind_df, pd.DataFrame)
+
+
+def test_range_plot():
+    '''
+    Test range_plot method.
+    '''
+    assets = Assets()
+    p = Specification()
+    calc = Calculator(p, assets)
+    p2 = Specification(year=2026)
+    p2.update_specification({'CIT_rate': 0.25})
+    calc2 = Calculator(p2, assets)
+    fig = calc.range_plot(calc2)
+    assert fig
+    fig = calc.range_plot(calc2, output_variable='rho')
+    assert fig
+
+
+def test_grouped_bar():
+    '''
+    Test grouped_bar method.
+    '''
+    assets = Assets()
+    p = Specification()
+    calc = Calculator(p, assets)
+    p2 = Specification(year=2026)
+    p2.update_specification({'CIT_rate': 0.25})
+    calc2 = Calculator(p2, assets)
+    fig = calc.grouped_bar(calc2)
+    assert fig
+    fig = calc.grouped_bar(calc2, output_variable='rho',
+                           group_by_asset=False)
+    assert fig
+
+
+def test_asset_bubble():
+    '''
+    Test asset bubble plot method.
+    '''
+    assets = Assets()
+    p = Specification()
+    calc = Calculator(p, assets)
+    p2 = Specification(year=2026)
+    p2.update_specification({'CIT_rate': 0.25})
+    calc2 = Calculator(p2, assets)
+    fig = calc.asset_bubble(calc2)
+    assert fig
+    fig = calc.asset_bubble(calc2, output_variable='rho_mix')
+    assert fig
+
+
+def test_bubble_widget():
+    '''
+    Test asset bubble plot method.
+    '''
+    assets = Assets()
+    p = Specification()
+    calc = Calculator(p, assets)
+    p2 = Specification(year=2026)
+    p2.update_specification({'CIT_rate': 0.25})
+    calc2 = Calculator(p2, assets)
+    fig = calc.bubble_widget(calc2)
+    assert fig
