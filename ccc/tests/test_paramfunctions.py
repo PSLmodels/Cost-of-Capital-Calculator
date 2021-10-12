0f9a61ae95ec41870276c8aa9b38da75ddaefab1
duncanhobbs <duncan2012@gmail.com>
2021-10-12 09:21:52 -0400

    Replace _nc with _pt in test_paramfunctions.py.

--- ccc/tests/test_paramfunctions.py
+++ ccc/tests/test_paramfunctions.py
@@ -93,44 +93,44 @@ revisions = {'Y_td': 8, 'Y_scg': 2, 'Y_lcg': 7, 'tau_td': 0.2,
              'E_c': 0.09, 'nominal_interest_rate': 0.08,
              'inflation_rate': 0.02, 'alpha_c_d_ft': 0.3,
              'alpha_c_d_td': 0.5, 'alpha_c_d_nt': 0.2,
-             'alpha_nc_d_ft': 0.5, 'alpha_nc_d_td': 0.4,
-             'alpha_nc_d_nt': 0.1, 'alpha_c_e_ft': 0.6,
+             'alpha_pt_d_ft': 0.5, 'alpha_pt_d_td': 0.4,
+             'alpha_pt_d_nt': 0.1, 'alpha_c_e_ft': 0.6,
              'alpha_c_e_td': 0.3, 'alpha_c_e_nt': 0.1, 'omega_scg': 0.7,
              'omega_lcg': 0.1, 'omega_xcg': 0.2, 'f_c': 0.32,
-             'f_nc': 0.42}
+             'f_pt': 0.42}
 p.update_specification(revisions)
 expected_dict = {
     'c': {'mix': 0.042792929, 'd': 0.029696442, 'e': 0.048955981},
-    'nc': {'mix': 0.027511674, 'd': 0.025517154, 'e': 0.028955981}}
+    'pt': {'mix': 0.027511674, 'd': 0.025517154, 'e': 0.028955981}}
 
 
 @pytest.mark.parametrize('entity_type,p,expected_dict',
                          [('c', p, expected_dict),
-                          ('nc', p, expected_dict)],
+                          ('pt', p, expected_dict)],
                          ids=['Corporate', 'Pass-Throughs'])
 def test_calc_s(entity_type, p, expected_dict):
     '''
     Test of the paramfunctions.calc_s function
     '''
-    test_dict, test_E_nc = pf.calc_s(p)
+    test_dict, test_E_pt = pf.calc_s(p)
 
     for k, v in test_dict[entity_type].items():
         assert(np.allclose(v, expected_dict[entity_type][k]))
 
-    assert(np.allclose(test_E_nc, 0.048955981))
+    assert(np.allclose(test_E_pt, 0.048955981))
 
 
 p = Specification()
 revisions = {
-    'f_c': 0.4, 'f_nc': 0.2, 'interest_deduct_haircut_corp': 0.0,
-    'interest_deduct_haircut_PT': 0.0, 'ace_c': 0.0, 'ace_nc': 0.0,
-    'CIT_rate': 0.25, 'tau_nc': 0.22, 'nominal_interest_rate': 0.05,
+    'f_c': 0.4, 'f_pt': 0.2, 'interest_deduct_haircut_c': 0.0,
+    'interest_deduct_haircut_pt': 0.0, 'ace_c': 0.0, 'ace_pt': 0.0,
+    'CIT_rate': 0.25, 'tau_pt': 0.22, 'nominal_interest_rate': 0.05,
     'inflation_rate': 0.02, 'E_c': 0.08}
 p.update_specification(revisions)
 expected_r_dict = {
     'c': {'mix': np.array([0.075]), 'd': np.array([0.0375]),
           'e': np.array([0.1])},
-    'nc': {'mix': np.array([0.0798]), 'd': np.array([0.039]),
+    'pt': {'mix': np.array([0.0798]), 'd': np.array([0.039]),
            'e': np.array([0.09])}}
 
 
@@ -142,12 +142,12 @@ def test_calc_r(p, expected_dict):
     Test of the calculation of the discount rate function
     '''
     f_dict = {'c': {'mix': p.f_c, 'd': 1.0, 'e': 0.0},
-              'nc': {'mix': p.f_nc, 'd': 1.0, 'e': 0.0}}
-    int_haircut_dict = {'c': p.interest_deduct_haircut_corp,
-                        'nc': p.interest_deduct_haircut_PT}
-    E_dict = {'c': p.E_c, 'nc': 0.07}
+              'pt': {'mix': p.f_pt, 'd': 1.0, 'e': 0.0}}
+    int_haircut_dict = {'c': p.interest_deduct_haircut_c,
+                        'pt': p.interest_deduct_haircut_pt}
+    E_dict = {'c': p.E_c, 'pt': 0.07}
     print('E dict = ', E_dict)
-    ace_dict = {'c': p.ace_c, 'nc': p.ace_nc}
+    ace_dict = {'c': p.ace_c, 'pt': p.ace_pt}
     test_dict = pf.calc_r(p, f_dict, int_haircut_dict, E_dict, ace_dict)
 
     for k, v in test_dict.items():
@@ -158,7 +158,7 @@ def test_calc_r(p, expected_dict):
 expected_rprime_dict = {
     'c': {'mix': np.array([0.08]), 'd': np.array([0.05]),
           'e': np.array([0.1])},
-    'nc': {'mix': np.array([0.082]), 'd': np.array([0.05]),
+    'pt': {'mix': np.array([0.082]), 'd': np.array([0.05]),
            'e': np.array([0.09])}}
 
 
@@ -170,8 +170,8 @@ def test_calc_rprime(p, expected_dict):
     Test of the calculation of the after-tax rate of return function
     '''
     f_dict = {'c': {'mix': p.f_c, 'd': 1.0, 'e': 0.0},
-              'nc': {'mix': p.f_nc, 'd': 1.0, 'e': 0.0}}
-    E_dict = {'c': p.E_c, 'nc': 0.07}
+              pt: {'mix': p.f_pt, 'd': 1.0, 'e': 0.0}}
+    E_dict = {'c': p.E_c, 'pt': 0.07}
     test_dict = pf.calc_r_prime(p, f_dict, E_dict)
 
     for k, v in test_dict.items():
