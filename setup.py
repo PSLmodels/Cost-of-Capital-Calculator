2c2dfae9c271bc5aabfe641d78894129ced7dc44
jdebacker <jason.debacker@gmail.com>
2024-09-15 21:09:06 -0400

    update setup.py format

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
-with open("README.md") as f:
-    longdesc = f.read()
+import setuptools
 
-version = "1.5.0"
+with open("README.md", "r", encoding="utf-8") as fh:
+    longdesc = fh.read()
 
-config = {
-    "description": "CCC: A Cost of Capital Calculator",
-    "url": "https://github.com/PSLmodels/Cost-of-Capital-Calculator",
-    "download_url": "https://github.com/PSLmodels/Cost-of-Capital-Calculator",
-    "long_description_content_type": "text/markdown",
-    "long_description": longdesc,
-    "version": version,
-    "license": "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-    "packages": ["ccc"],
-    "include_package_data": True,
-    "name": "cost-of-capital-calculator",
-    "install_requires": ["taxcalc", "pandas", "bokeh", "numpy", "paramtools"],
-    "classifiers": [
+setuptools.setup(
+    description="CCC: A Cost of Capital Calculator",
+    url="https://github.com/PSLmodels/Cost-of-Capital-Calculator",
+    download_url="https://github.com/PSLmodels/Cost-of-Capital-Calculator",
+    long_description_content_type="text/markdown",
+    long_description=longdesc,
+    version="1.5.1",
+    license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    packages=["ccc"],
+    include_package_data=True,
+    name="cost-of-capital-calculator",
+    install_requires=["taxcalc", "pandas", "bokeh", "numpy", "paramtools"],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Natural Language :: English",
@@ -28,12 +24,10 @@ config = {
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    "tests_require": ["pytest"],
-}
-
-setup(**config)
+    tests_require=["pytest"],
+)
