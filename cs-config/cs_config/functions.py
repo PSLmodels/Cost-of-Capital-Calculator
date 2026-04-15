1c88a4eae3f3e974cabe7fb62a28ce6b02652fb7
Jason DeBacker <jason.debacker@gmail.com>
2026-04-15 15:22:46 -0400

    fix deprecated call to puf weights

--- cs-config/cs_config/functions.py
+++ cs-config/cs_config/functions.py
@@ -14,6 +14,9 @@ import cs2tc
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+PUF_WEIGHTS_S3_FILE_LOCATION = os.environ.get(
+    "PUF_WEIGHTS_S3_LOCATION", "s3://ospc-data-files/puf_weights.20210720.csv.gz"  ## This needs to be updated
+)
 PUF_S3_FILE_LOCATION = os.environ.get(
     "PUF_S3_LOCATION", "s3://ospc-data-files/puf.20210720.csv.gz"
 )
@@ -184,7 +187,9 @@ def run_model(meta_param_dict, adjustment):
         data = retrieve_puf(
             PUF_S3_FILE_LOCATION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
         )
-        weights = Records.PUF_WEIGHTS_FILENAME
+        weights = retrieve_puf(
+            PUF_WEIGHTS_S3_FILE_LOCATION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+        )
         records_start_year = Records.PUFCSV_YEAR
         if data is not None:
             if not isinstance(data, pd.DataFrame):
@@ -208,7 +213,7 @@ def run_model(meta_param_dict, adjustment):
             meta_params.adjust({"data_source": "CPS"})
     elif meta_params.data_source == "CPS":
         data = "cps"
-        weights = Records.PUF_WEIGHTS_FILENAME
+        weights = os.path.join(Records.CODE_PATH, 'cps_weights.csv.gz')
         records_start_year = Records.CPSCSV_YEAR
     else:
         raise ValueError(
